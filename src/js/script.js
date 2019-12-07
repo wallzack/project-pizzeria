0fbdd5123b9e8509284afbce4d5c1e4c9e238e03
wallzack <vulcheek@gmail.com>
2019-12-07 01:13:48 +0100

    Added price calculating on default and optional pizza addons

--- src/js/script.js
+++ src/js/script.js
@@ -58,8 +58,11 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
-      console.log('new Product:', thisProduct);
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+      //console.log('new Product:', thisProduct);
     }
 
     renderInMenu(){
@@ -78,6 +81,16 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
 
@@ -85,8 +98,8 @@
       const clickableTrigger = thisProduct.element;
 
       /* START: click event listener to trigger */
-      clickableTrigger.addEventListener('click', function(event){
-        console.log('clicked trigger: ', clickableTrigger);
+      thisProduct.accordionTrigger.addEventListener('click', function(){
+        // console.log('clicked trigger: ', clickableTrigger);
 
         /* prevent default action for event */
         event.preventDefault();
@@ -96,7 +109,7 @@
 
         /* find all active products */
         const activeProducts = document.querySelectorAll('.active');
-        console.log('found active products: ', activeProducts);
+        //console.log('found active products: ', activeProducts);
 
         /* START LOOP: for each active product */
         for(let activeProduct of activeProducts){
@@ -114,12 +127,84 @@
       /* END: click event listener to trigger */
       });
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      /* read all data from the form */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      //console.log('formData', formData);
+
+      /* Add new empty object */
+      thisProduct.params = {};
+
+      /* set variable price to equal thisProduct.data.price */
+      let price = thisProduct.data.price;
+      console.log('Product whole price: ', price);
+
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in thisProduct.data.params){
+
+        /* save element in thisProduct.data.params with key paramId as const param */
+        const param = thisProduct.data.params[paramId];
+
+        /* START LOOP: for each optionId in param.options */
+        for (let optionId in param.options){
+
+          /* save element in param.otions with key optionId as const option */
+          const option = param.options[optionId];
+
+          /*  START IF condition: if option is selected and option is not default */
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+          console.log('option selected: ', optionSelected);
+
+          if(optionSelected && !option.default){
+
+            /* add price of option to variable price */
+            price = price + option.price;
+            console.log('price: ', price);
+
+            /* END IF condition: if option is selected and option is not default */
+          }
+
+          /* START ELSE IF: if option is not selected and option is default */
+          else if (!optionSelected && option.default){
+            /* deduct price of option from price */
+            price = price - option.price;
+            console.log('price deducted: ', price);
+          
+          }
+        }
+      }
+
+    }
+
   }
 
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      //console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
@@ -134,11 +219,11 @@
 
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      // console.log('*** App starting ***');
+      // console.log('thisApp:', thisApp);
+      // console.log('classNames:', classNames);
+      // console.log('settings:', settings);
+      // console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
