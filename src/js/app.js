9571e5bf3006b4aa3a0eefcf9630e1cc26c79585
wallzack <vulcheek@gmail.com>
2019-12-19 23:32:51 +0100

    10.2 module 2nd video: cart stopped working

--- src/js/app.js
+++ src/js/app.js
@@ -8,24 +8,55 @@ const app = {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-    thisApp.activatePage(thisApp.page[0].id);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    console.log('idFromHash', idFromHash);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash == page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with this id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
   },
+
 
   activatePage: function(pageId){
     const thisApp = this;
 
     /* add classs "active" to matching pages, remove from non-matching */
     for(let page of thisApp.pages){
-      // if(page.id == pageId){
-      //   page.classList.add(classNames.pages.active);
-      // } else {
-      //   page.classList.remove(classNames.pages.active);
-      // }
-
-      page.classList.toggle(classNames.pages.active);
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
     /* add classs "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
   },
 
   initMenu: function(){
