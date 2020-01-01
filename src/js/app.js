7743a7d437721983b499ee387af33894fc31d1f2
wallzack <vulcheek@gmail.com>
2020-01-01 23:24:09 +0100

    11.3 module: added home page css. Redirect and carousel don't work.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
+import Home from './components/Home.js';
 
 
 const app = {
@@ -113,6 +114,68 @@ const app = {
     // console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
+  initCarousel() {
+    // eslint-disable-next-line no-unused-vars
+
+    const review = [];
+
+    review[0] = {
+      title: 'Nulla',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      author: 'Efficitur N.',
+
+    };
+    review[1] = {
+      title: 'Vestibulum !!!',
+      content: 'Aenean vitae quam suscipit, interdum arcu nec,',
+      author: 'Pellentesque A.',
+    };
+    review[2] = {
+      title: 'Lobortis ???',
+      content: 'Mauris maximus ipsum sed.',
+      author: 'Vel F.',
+    };
+    let i = 0;
+    console.log(review[0]);
+    //console.log(review);
+
+    const dots = document.querySelectorAll('.carousel-dots i');
+    console.log(dots);
+
+    function changeTitle() {
+
+      const title = document.querySelector('.review-title');
+      console.log(title);
+      const content = document.querySelector('.reviev-content');
+      console.log(content);
+      const author = document.querySelector('.review-author');
+      console.log(author);
+
+      for (let dot of dots) {
+        if (dot.id == i + 1) { // +1 ??
+          console.log(dot.id);
+          dot.classList.add('active');
+        } else {
+          dot.classList.remove('active');
+        }
+        title.innerHTML = review[i].title;
+        content.innerHTML = review[i].content;
+        author.innerHTML = review[i].author;
+      }
+
+      if (i < review.length - 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+    }
+    changeTitle();
+
+    setInterval(() => {
+      changeTitle();
+    }, 3000);
+  },
+
   init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -128,6 +191,8 @@ const app = {
     thisApp.initCart();
 
     thisApp.initBooking();
+
+    thisApp.initCarousel();
   },
 };
 
---
+++ src/js/components/Home.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import {settings} from '../settings.js';
+
+class Home {
+  constructor() {
+    // eslint-disable-next-line no-unused-vars
+    const thisHome = this;
+  }
+}
+export default Home;
